d400e6929a4e90599d9a792d4fffd46d250101e3
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-09-14 18:20:45 -0400

    pass tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, name){
+    if (!arr.length){
+        return []
+    };
+
+    return arr.filter(element => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(arr, name){
+    return arr.filter(driver => driver.startsWith(name));
+}
+
+function matchName(arr, name){
+    return arr.filter(x => x.name === name)
+}
